fde0c5ce3db85d4b6fcc4d90793c6ba523aeb964
justshipmore <justshipmore@gmail.com>
2024-10-27 20:07:34 +0000

    Fixed failing alias imports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@/': path.resolve(__dirname, './src/'),
+      '@': path.resolve(__dirname, './src/'),
       '@routes': path.resolve(__dirname, './src/routes/'),
       '@pages': path.resolve(__dirname, './src/pages/'),
       '@features': path.resolve(__dirname, './src/features/')
