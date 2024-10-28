a34fdc92d2bfb6767c7fd60dfedefaa081f87583
justshipmore <justshipmore@gmail.com>
2024-10-28 19:12:06 +0000

    Fixed spelling mistake2

--- frontend/src/features/list/components/list-item.tsx
+++ frontend/src/features/list/components/list-item.tsx
@@ -6,7 +6,7 @@ const ListItem = () => (
       <div className="h-96 bg-blue-500 p-8 rounded-lg"></div>
       <div className="flex flex-col py-4">
         <span className="text-muted-foreground">Jane Smith</span>
-        <span className="text-lg">The Chronicals of Nania</span>
+        <span className="text-lg">The Chronicles of Narnia</span>
       </div>
     </div>
   </Link>
